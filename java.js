4624329f4b72981f055d8171a5ca3569a65ef4e0
tomisonrix <124640136+tomisonrix@users.noreply.github.com>
2023-03-05 23:18:03 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,10 @@
+var Nombre = "Nombre" 
+var Apellido = "Apellido"
+
+console.log(Nombre + Apellido);
+
+var numeroa = 5
+var numerob = 5
+var suma = console.log(5 + 5)
+
+var numerica = console.log(suma + 5) 
